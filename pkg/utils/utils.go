f259d20a1bd89f1d20fa52033bb72bbb9de3e3fb
Ali <ali.zolghadr@gmail.com>
2024-02-05 12:38:24 +1100

    Add func ParseBody

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,1 +1,18 @@
 package utils
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func ParseBody(r *http.Request, data interface{}) error {
+	defer r.Body.Close()
+
+	if body, err := io.ReadAll(r.Body); err == nil {
+		if err := json.Unmarshal([]byte(body), data); err != nil {
+			return err
+		}
+	}
+	return nil
+}
