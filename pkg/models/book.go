20fcc9e9f2c81ce5afb44c9bc7c81793a5d6715c
Ali <ali.zolghadr@gmail.com>
2024-02-05 12:51:12 +1100

    Add Book struct and func init

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,1 +1,21 @@
 package models
+
+import (
+	"github.com/Aliqc7/Bookstore/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model         // gorm.model
+	Name        string `gorm:"" json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
